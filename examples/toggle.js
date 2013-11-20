1c886299e8c23127b5f58398257ff8a814705718
Dom Christie <christiedom@gmail.com>
2013-11-20 10:37:45 +0000

    Improve click/touch detection

--- examples/toggle.js
+++ examples/toggle.js
@@ -1,5 +1,7 @@
 (function() {
-  var clickType = document.ontouchstart !== null ? 'click' : 'touchstart';
+  var clickType = ('ontouchstart' in document.documentElement ||
+    window.navigator.msPointerEnabled) ? 'touchstart' : 'click';
+  
   $(document).on(clickType, '[data-drawer-toggle]', function(evt) {
     evt.preventDefault();
     $('body').toggleClass('drawer-open');
