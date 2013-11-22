0825123ccdaff3a0321cd8c416123a5dd7c2e00e
Dom Christie <christiedom@gmail.com>
2013-11-22 13:56:25 +0000

    Remove html,body{position:relative} styles to fix short drawer-butt case. Remove position:absolute styles in examples

--- examples/toggle.js
+++ examples/toggle.js
@@ -7,29 +7,5 @@
     if(isTouch && evt.type === 'click') return;
 
     $('html').toggleClass('drawer-open');
-    $('.drawer-butt').trigger('transitionstart');
   });
-
-  $(document).on('transitionend', '.drawer-butt', function() {
-    if(!drawerOpen()) {
-      $('.drawer').each(function(i, el) {
-        el.style.webkitOverflowScrolling = null;
-      });
-      $('body').focus();
-      $(this).off('touchmove.drawer')
-    }
-  });
-
-  $(document).on('transitionstart', '.drawer-butt', function() {
-    if(drawerOpen()) {
-      $('.drawer').each(function(i, el) {
-        el.style.webkitOverflowScrolling = 'touch';
-      }).focus();
-      $(this).on('touchmove.drawer', function(evt) { return false; });
-    }
-  });
-
-  function drawerOpen() {
-    return !!$('.drawer-open').length;
-  }
- })()
+})();
